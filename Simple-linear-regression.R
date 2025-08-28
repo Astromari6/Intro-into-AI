924780c02bc905940d80f9c47ef7fbb6e3169b63
Astromari6 <woodkari069@gmail.com>
2025-08-28 14:49:38 -0400

    Add files via upload

---
+++ Simple-linear-regression.R
@@ -0,0 +1,7 @@
+#Author: Wood; Date: Aug 28, 2025; Purpose: Simple linear regression
+
+#Get dummy dataset
+cars <-mtcars
+
+#Scatter plot on training data
+scatter.smooth(x=cars$mpg, y=car$disp, min="SpeedVSdistance")
