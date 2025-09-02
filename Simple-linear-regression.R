caae9c47454b68cdeaeb49882baa92f0c0b68e48
Astromari6 <woodkari069@gmail.com>
2025-09-02 14:54:04 -0400

    Add files via upload

--- Simple-linear-regression.R
+++ Simple-linear-regression.R
@@ -1,7 +1,16 @@
-#Author: Wood; Date: Aug 28, 2025; Purpose: Simple linear regression
+# Author: Wood; Date: Aug 28, 2025; Purpose: Simple linear regression
 
-#Get dummy dataset
+# Get dummy dataset
 cars <-mtcars
 
-#Scatter plot on training data
+# Scatter plot on training data
 scatter.smooth(x=cars$mpg, y=car$disp, min="SpeedVSdistance")
+
+# Calculate correlation value between mpg and disp
+cor(cars$mpg, cars$disp)
+
+# Split training and testing data for regression
+training_data <- cars[1:16,]
+test_data <- cars[17:32,]
+
+# Build a regression model
