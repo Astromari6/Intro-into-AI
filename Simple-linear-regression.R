fb7a3e3a7a505baf1130d6a12351a46e92c19da5
Astromari6 <woodkari069@gmail.com>
2025-09-04 14:39:39 -0400

    Add files via upload

--- Simple-linear-regression.R
+++ Simple-linear-regression.R
@@ -1,4 +1,4 @@
-# Author: Wood; Date: Aug 28, 2025; Purpose: Simple linear regression
+cars,-or: Wood; Date: Aug 28, 2025; Purpose: Simple linear regression
 
 # Get dummy dataset
 cars <-mtcars
@@ -14,3 +14,10 @@ training_data <- cars[1:16,]
 test_data <- cars[17:32,]
 
 # Build a regression model
+regression_result <- lm(mpg ~ disp, data=training_data)
+
+# Predict regression function on test data
+prediction_result <- predict(regression_result, test_data)
+
+# Results of prediction
+actual_prediction_values <- data.frame(cbind(actuals=training_data$mpg, predicteds=prediction_result))
