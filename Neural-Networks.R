bde082968cdf9384c4b512320c4ccd0e833c90ec
Astromari6 <woodkari069@gmail.com>
2025-11-06 14:44:16 -0500

    Add files via upload

---
+++ Neural-Networks.R
@@ -0,0 +1,15 @@
+#Author: Wood; Date 11/06/2025; Purpose: Implement Neural Networks
+
+#Load library Caret
+library(caret)
+
+#Load sample dataset
+dataset <- iris
+
+#80% split for training data and 20% split for validation data
+
+validation_index <- createDataPartition(dataset$Species, p=0.80,
+list=FALSE)
+
+validation <- dataset[-validation_index,]
+dataset <- dataset[validation_index,]
